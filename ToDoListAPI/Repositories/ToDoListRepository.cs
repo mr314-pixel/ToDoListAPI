aa9c03fb334fb7c7ee729d868b3516ec5bcc7f61
mr314-pixel <cambourne71@gmail.com>
2024-11-20 00:05:37 +0000

    Clean up testing code

--- ToDoListAPI/Repositories/ToDoListRepository.cs
+++ ToDoListAPI/Repositories/ToDoListRepository.cs
@@ -4,34 +4,6 @@ namespace ToDoListAPI.Repositories
 {
     public class ToDoListRepository : IToDoListRepository
     {
-        public ToDoListRepository()
-        {
-            using (var context = new ApiContext())
-            {
-                var items = new List<ToDoItem>
-                {
-                    new ToDoItem
-                    {
-                        ToDoListId = 0,
-                        Title = "Demo Item 1 Title",
-                        Description = "Demo Item 1 Description",
-                        CreatedDate = DateTime.UtcNow,
-                        CompletedDate = null
-                    },
-                    new ToDoItem
-                    {
-                        ToDoListId = 0,
-                        Title = "Demo Item 2 Title",
-                        Description = "Demo Item 2 Description",
-                        CreatedDate = DateTime.UtcNow.AddDays(-2),
-                        CompletedDate = null
-                    }
-                };
-                context.ToDoItems.AddRange(items);
-                context.SaveChanges();
-            }
-        }
-
         public List<ToDoItem> GetToDoItems(List<int> toDoItemId, int toDoListId)
         {
             using (var context = new ApiContext())
